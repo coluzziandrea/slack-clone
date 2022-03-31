873c5add1ab4eeef5b766d8faf6333a52e45722d
Andrea Coluzzi <coluzziandrea.dev@gmail.com>
2022-03-31 23:40:16 +0200

    added sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import styled from 'styled-components';
 
 import './App.css';
 import Header from './components/Header/Header';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className='app'>
-      <Header></Header>
-      <Routes>
-        <Route path='/' element={<h1> Home </h1>} />
-      </Routes>
+      <Header />
+      <AppBody>
+        <Sidebar />
+        <Routes>
+          <Route path='/' element={<h1> Home </h1>} />
+        </Routes>
+      </AppBody>
     </div>
   );
 }
 
 export default App;
+
+const AppBody = styled.div``;
---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,7 @@
+import { SidebarContainer } from './Sidebar.style';
+
+function Sidebar() {
+  return <SidebarContainer></SidebarContainer>;
+}
+
+export default Sidebar;
