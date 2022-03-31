1c45751e5ec49c38d34759338a42c7070388082d
Andrea Coluzzi <coluzziandrea.dev@gmail.com>
2022-03-31 14:49:36 +0200

    added reac router & firebase config

---
+++ src/firebase.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDh5Npgk1Qe3ZXG_4ALIq6PPqV9Hit50qg',
+  authDomain: 'slack-clone-coluz.firebaseapp.com',
+  projectId: 'slack-clone-coluz',
+  storageBucket: 'slack-clone-coluz.appspot.com',
+  messagingSenderId: '898839139548',
+  appId: '1:898839139548:web:b4b28a2a10ef94a8b95019',
+};
